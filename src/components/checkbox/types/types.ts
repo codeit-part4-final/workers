6c19cf84aefbb7aec8739717333e0c1b6dc1a9d6
jieunsse <jieunsse@gmail.com>
2026-01-30 15:29:47 +0900

    refactor: 체크박스 동적으로 처리 가능하도록 타입 개선

--- src/components/checkbox/types/types.ts
+++ src/components/checkbox/types/types.ts
@@ -1,24 +1,39 @@
+import type { ReactNode } from 'react';
+
 export type CheckBoxSize = 'large' | 'small';
 
+export type CheckBoxIconSet = {
+  checked: ReactNode;
+  unchecked: ReactNode;
+};
+
+export type CheckBoxOptions = {
+  ariaLabel?: string;
+  readOnly?: boolean;
+  icons?: CheckBoxIconSet;
+};
+
+export type CheckBoxOptionsWithAriaLabel = Omit<CheckBoxOptions, 'ariaLabel'> & {
+  ariaLabel: string;
+};
+
 interface CheckBoxBaseProps {
-  isChecked: boolean;
+  checked: boolean;
   size?: CheckBoxSize;
   id?: string;
   name?: string;
   value?: string;
   disabled?: boolean;
   className?: string;
-  onChange?: (isChecked: boolean) => void;
+  onCheckedChange?: (checked: boolean) => void;
 }
 
-export interface CheckBoxPropsWithLabel extends CheckBoxBaseProps {
-  label: string;
-  ariaLabel?: string;
-}
-
-export interface CheckBoxPropsWithAriaLabel extends CheckBoxBaseProps {
-  label?: undefined;
-  ariaLabel: string;
-}
-
-export type CheckBoxProps = CheckBoxPropsWithLabel | CheckBoxPropsWithAriaLabel;
+export type CheckBoxProps =
+  | (CheckBoxBaseProps & {
+      label: ReactNode;
+      options?: CheckBoxOptions;
+    })
+  | (CheckBoxBaseProps & {
+      label?: undefined;
+      options: CheckBoxOptionsWithAriaLabel;
+    });
