06f4714d20f2318c712a9510d1b09c399abf9e93
jieunsse <jieunsse@gmail.com>
2026-02-02 17:16:58 +0900

    feat: 브리핑용 스토리 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,14 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 
-export default defineConfig([
-  ...nextVitals,
-  ...nextTs,
+export default defineConfig([...nextVitals, ...nextTs, {
+  rules: {
+    'prefer-const': 'error',
 
-  {
-    rules: {
-      'prefer-const': 'error',
-
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    },
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
   },
-
-  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
-]);
+}, globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']), ...storybook.configs["flat/recommended"]]);
