09fd056900f6ced54b39b874656f942e94ef3b76
jieunsse <jieunsse@gmail.com>
2026-02-22 15:25:14 +0900

    refactor: 대시보드 리팩토링

--- src/app/[teamid]/_domain/components/Team/TeamDashboard.tsx
+++ src/app/[teamid]/_domain/components/Team/TeamDashboard.tsx
@@ -1,26 +1,17 @@
 'use client';
 
-import { useState } from 'react';
 import { useParams } from 'next/navigation';
 import KanbanBoard from '../Kanban/KanbanBoard';
 import TodayReport from '../TodayReport/TodayReport';
 import MemberSection from '../Member/MemberSection';
 import styles from './TeamDashboard.module.css';
-import {
-  MOCK_MEMBERS,
-  MOCK_TASKS,
-  MOCK_TODAY_REPORT,
-  MOCK_TEAM_NAME,
-} from '../../constants/mockData';
-import type { KanbanTask } from '../../interfaces/team';
+import { MOCK_MEMBERS, MOCK_TODAY_REPORT, MOCK_TEAM_NAME } from '../../constants/mockData';
 
 const isAdmin = true;
 
 export default function TeamDashboard() {
   const params = useParams<{ teamid: string }>();
   const teamid = params?.teamid ?? '1';
-
-  const [tasks, setTasks] = useState<KanbanTask[]>(MOCK_TASKS);
 
   return (
     <div className={styles.container}>
@@ -35,7 +26,7 @@ export default function TeamDashboard() {
 
       <div className={styles.content}>
         <div className={styles.kanbanArea}>
-          <KanbanBoard tasks={tasks} setTasks={setTasks} teamId={teamid} />
+          <KanbanBoard teamId={teamid} />
         </div>
 
         <aside className={styles.rightPanel}>
