da9573dc6504435374831779783bb53043ef5ba3
jieunsse <jieunsse@gmail.com>
2026-02-23 00:37:29 +0900

    style: [teamid] 레이아웃 크기 확대 및 사이드바 여백 추가

--- src/app/[teamid]/_domain/components/Team/TeamDashboard.tsx
+++ src/app/[teamid]/_domain/components/Team/TeamDashboard.tsx
@@ -80,6 +80,7 @@ export default function TeamDashboard() {
   const handleConfirmDelete = () => {
     deleteGroup(undefined, {
       onSuccess: () => {
+        setIsDeleteModalOpen(false);
         // 삭제된 팀을 제외한 나머지 멤버십에서 첫 번째 팀으로 이동, 없으면 addteam 페이지로 이동
         const remainingTeams = (currentUser?.memberships ?? [])
           .filter((m) => m.groupId !== groupId)
