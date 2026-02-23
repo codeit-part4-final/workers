7fc752258611a74dc53d4a8b773a578500974628
jieunsse <jieunsse@gmail.com>
2026-02-23 11:57:01 +0900

    feat: useParam 통해서 리다이렉팅 대응

--- src/app/[teamid]/page.tsx
+++ src/app/[teamid]/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import { useParams } from 'next/navigation';
 
-// SSR을 비활성화하여 서버에서 인증 없이 API 호출하는 문제를 방지
-// [teamid] 페이지는 로그인 이후에만 접근하는 인증 필요 페이지
 const TeamDashboard = dynamic(() => import('./_domain/components/Team/TeamDashboard'), {
   ssr: false,
 });
 
 export default function TeamPage() {
-  return <TeamDashboard />;
+  const params = useParams<{ teamid: string }>();
+
+  return <TeamDashboard key={params?.teamid} />;
 }
