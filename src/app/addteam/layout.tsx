bda1f0e30be985e64e7a006e7cec210282c8f8dc
jieunsse <jieunsse@gmail.com>
2026-02-23 17:46:26 +0900

    feat: 사이드바 프로필 클릭 시 /mypage로 라우팅 추가

--- src/app/addteam/layout.tsx
+++ src/app/addteam/layout.tsx
@@ -1,10 +1,11 @@
-import { MobileHeader, Sidebar } from '@/components/sidebar';
+import { MobileHeader } from '@/components/sidebar';
+import AddTeamSidebarWrapper from './_domain/components/AddTeamSidebarWrapper';
 import styles from './page.module.css';
 
 export default function AddTeamLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className={styles.page}>
-      <Sidebar />
+      <AddTeamSidebarWrapper />
       <div className={styles.mobileOnlyHeader}>
         <MobileHeader />
       </div>
